1554086e7f744425267751a98d8bb58181eabf27
lc_666 <l_alu@icloud.com>
2021-10-01 23:32:31 +0800

    add strings.py.

--- strings.py
+++ strings.py
@@ -14,3 +14,41 @@ print('My name is {name}, and I am {age} years old'.format(name=name, age=age))
 
 # String Formatting 2: F-Strings
 print(f'My name is {name}, and I am {age} years old.')
+
+# Methods
+# Capitalize
+print('hello'.capitalize())
+
+# Upper Lower
+print('demo'.upper())
+print('LC'.lower())
+
+# Swap case
+print('abedEFG'.swapcase())
+
+# Get length
+print(len('marvel'))
+
+# Count
+print('abide'.count('a'))
+
+# Replace
+print('John'.replace('n', 'e'))
+
+# Starts with/End With
+print("I Love You".startswith('I'))
+
+# Split into a list
+print('HelloWorld'.split('o'))
+
+# Is all alphanumeric
+print('123'.isalnum())
+
+# Is all alphabetic
+print('123a'.isalpha())
+
+# Is all numeric
+print('123b'.isnumeric())
+
+# List
+print(list('Hello W'))
